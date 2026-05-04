f9f33221df7662cff6bce9de1aa4783b85884f20
Natnael Habteselassie <natnaelhabteselassiedemissie@gmail.com>
2026-05-04 10:01:33 +0300

    Add terms of service page with dummy appbar

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,13 +1,19 @@
 import 'package:go_router/go_router.dart';
+import '../../features/common/screens/terms.dart';
 import '../../features/common/screens/about_us.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/about_us',
+  initialLocation: '/terms',
   routes: [
     GoRoute(
       path: '/about_us',
       name: 'aboutUs',
       builder: (context, state) => const AboutUs(),
+    ),
+    GoRoute(
+      path: '/terms',
+      name: 'terms',
+      builder: (context, state) => const Terms(),
     ),
   ],
 );
