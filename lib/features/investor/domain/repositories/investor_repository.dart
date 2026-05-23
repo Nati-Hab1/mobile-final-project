6d8411128e8b84e48f0a48d46b4040b33fab5dfd
Natnael Habteselassie <natnaelhabteselassiedemissie@gmail.com>
2026-05-23 16:58:31 +0300

    Update investor repo and get bookmarks usecase

--- lib/features/investor/domain/repositories/investor_repository.dart
+++ lib/features/investor/domain/repositories/investor_repository.dart
@@ -5,21 +5,27 @@ import '../entities/startup.dart';
 import '../entities/interest.dart';
 
 abstract class InvestorRepository {
-  Future<InvestorStats> getDashboardStats();
-  Future<List<InvestorStartup>> getAllStartups();
+  Future<InvestorStats> getDashboardStats(
+      {bool forceRefresh = false});
 
-  Future<List<Bookmark>> getBookmarks();
+  Future<List<InvestorStartup>> getAllStartups(
+      {bool forceRefresh = false});
+
+  Future<List<Bookmark>> getBookmarks(
+      {bool forceRefresh = false});
   Future<Bookmark> addBookmark(int startupId,
       {String? note});
   Future<void> deleteBookmark(int bookmarkId);
   Future<Bookmark> updateBookmarkNote(
       int bookmarkId, String note);
 
-  Future<List<InvestorInterest>> getInterests();
+  Future<List<InvestorInterest>> getInterests(
+      {bool forceRefresh = false});
   Future<InvestorInterest> expressInterest(int startupId,
       {String? message});
 
-  Future<List<InvestorIntro>> getIntros();
+  Future<List<InvestorIntro>> getIntros(
+      {bool forceRefresh = false});
   Future<void> acceptIntro(int introId);
   Future<void> declineIntro(int introId);
 }
